d72c7a4951c0a09e6cfc3ed8b68bb7ca7416d304
Joel / Junyong <likedyong@gmail.com>
2018-09-25 13:40:16 +0900

    Add atm

--- pkg/atm/atm.go
+++ pkg/atm/atm.go
@@ -1,30 +1,53 @@
 package atm
 
-import "github.com/dyong0/atm/pkg/account"
+import (
+	"github.com/dyong0/atm/pkg/atm/account"
+	"github.com/dyong0/atm/pkg/atm/account/medium"
+	"github.com/dyong0/atm/pkg/atm/currency"
+)
 
 type ATM struct {
-	accountService account.Service
+	accountRepo    account.Repository
+	currentAccount *account.Account
 }
 
-func (a *ATM) ReadAccount(id string, pw string) error {
-	err := a.accountService.ReadAccount(id, pw)
+func (a *ATM) ReadAccount(accMedium medium.Medium) error {
+	acc, err := a.accountRepo.Account(accMedium.AccountID())
+	if err != nil {
+		return err
+	}
+	err = a.accountRepo.VerifyAccount(acc, accMedium.Password())
 	if err != nil {
 		return err
 	}
 
+	a.currentAccount = acc
+
 	return nil
 }
 
-func (a *ATM) Deposit() {
-
+func (a *ATM) Deposit(amount currency.Amount) error {
+	return a.currentAccount.Deposit(amount)
 }
 
-func (a *ATM) Withdraw() {
-
+func (a *ATM) Withdraw(amount currency.Amount) (currency.Amount, error) {
+	return a.currentAccount.Withdraw(amount)
 }
 
-func New() *ATM {
-	return &ATM{
-		accountService: account.NewService(),
+func (a *ATM) Balance() uint32 {
+	return a.currentAccount.Balance()
+}
+
+func (a *ATM) Close() error {
+	a.currentAccount = nil
+	return nil
+}
+
+func NewATM() (*ATM, error) {
+	repo, err := account.NewRepository()
+	if err != nil {
+		return nil, err
 	}
+
+	return &ATM{accountRepo: repo}, nil
 }
--- pkg/atm/atm_test.go
+++ pkg/atm/atm_test.go
@@ -3,32 +3,70 @@ package atm
 import (
 	"testing"
 
-	"github.com/dyong0/atm/pkg/account"
+	"github.com/dyong0/atm/pkg/atm/account"
+	"github.com/dyong0/atm/pkg/atm/account/medium"
+	"github.com/dyong0/atm/pkg/atm/currency"
 )
 
 func TestATM(t *testing.T) {
-	atm := New()
+	atm, _ := NewATM()
 
-	mockAccService := &MockAccountService{
-		ReadAccountFunc: func(id string, pw string) error {
-			return account.ErrAccountNotFound
+	atm.accountRepo = &mockAccRepo{
+		AccountFunc: func(id string) (*account.Account, error) {
+			return account.NewAccount(currency.CurrencyYen), nil
+		},
+		VerifyAccountFunc: func(account *account.Account, pw string) error {
+			return nil
 		},
 	}
 
-	atm.accountService = mockAccService
+	atm.ReadAccount(card{id: "awesome", pw: "secret"})
 
-	err := atm.ReadAccount("non existing", "pw")
+	depositAmount, _ := currency.Yen(6000)
+	atm.Deposit(depositAmount)
+	if expect, got := uint32(6000), atm.Balance(); expect != got {
+		t.Errorf("Expect deposit amount of %d, got %d", expect, got)
+	}
 
-	if err != account.ErrAccountNotFound {
-		t.Error("Expect account to be not found")
+	withdrawAmount, _ := currency.Yen(651)
+	withdrawn, err := atm.Withdraw(withdrawAmount)
+	if expect, got := uint32(651), withdrawn.Total(); expect != got {
+		t.Errorf("Expect withdrawn %d, got %d", expect, got)
+	}
+	if err != nil {
+		t.Error("Expect no error while withdrawing")
+	}
+	if expect, got := uint32(6000-651), atm.Balance(); expect != got {
+		t.Errorf("Expect balance of %d, got %d", expect, got)
+	}
+
+	err = atm.Close()
+	if err != nil {
+		t.Error("Expect to close ATM successfully")
 	}
 }
 
-type MockAccountService struct {
-	account.Service
-	ReadAccountFunc func(string, string) error
+type mockAccRepo struct {
+	AccountFunc       func(id string) (*account.Account, error)
+	VerifyAccountFunc func(account *account.Account, pw string) error
 }
 
-func (s *MockAccountService) ReadAccount(id string, pw string) error {
-	return s.ReadAccountFunc(id, pw)
+func (r *mockAccRepo) Account(id string) (*account.Account, error) {
+	return r.AccountFunc(id)
+}
+func (r *mockAccRepo) VerifyAccount(account *account.Account, pw string) error {
+	return r.VerifyAccountFunc(account, pw)
+}
+
+type card struct {
+	medium.Medium
+	id string
+	pw string
+}
+
+func (c card) AccountID() string {
+	return c.id
+}
+func (c card) Password() string {
+	return c.pw
 }
