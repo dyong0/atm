648b1757a5c872bd5810229b524a1aae3af4cce8
Joel / Junyong <likedyong@gmail.com>
2018-09-27 16:57:54 +0900

    Add account media

--- internal/pkg/atm/account/medium/accountbook.go
+++ internal/pkg/atm/account/medium/accountbook.go
@@ -2,4 +2,13 @@ package medium
 
 type AccountBook struct {
 	Medium
+	accountID string
+	password  string
+}
+
+func (b AccountBook) AccountID() string {
+	return b.accountID
+}
+func (b AccountBook) Password() string {
+	return b.password
 }
--- internal/pkg/atm/account/medium/card.go
+++ internal/pkg/atm/account/medium/card.go
@@ -2,4 +2,22 @@ package medium
 
 type Card struct {
 	Medium
+	cardNo    string
+	accountID string
+	password  string
+}
+
+func NewCard(cardNo, password string) Card {
+	return Card{
+		cardNo:    cardNo,
+		accountID: cardNo,
+		password:  password,
+	}
+}
+
+func (c Card) AccountID() string {
+	return c.accountID
+}
+func (c Card) Password() string {
+	return c.password
 }
